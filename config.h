23d9c6b40b4e6bd1668317341359bb9bd9d044dd
yixy <youzhilane01@gmai.com>
2023-03-10 20:14:50 +0800

    add color msg patches
    Signed-off-by: yixy <youzhilane01@gmai.com>

--- config.h
+++ config.h
@@ -1,6 +1,6 @@
 /* user and group to drop privileges to */
 static const char *user  = "nobody";
-static const char *group = "nogroup";
+static const char *group = "nobody";
 
 static const char *colorname[NUMCOLS] = {
 	[INIT] =   "black",     /* after initialization */
@@ -10,3 +10,12 @@ static const char *colorname[NUMCOLS] = {
 
 /* treat a cleared input like a wrong password (color) */
 static const int failonclear = 1;
+
+/* default message */
+static const char * message = "Follow the white rabbit.";
+
+/* text color */
+static const char * text_color = "#ffffff";
+
+/* text size (must be a valid size) */
+static const char * font_name = "lucidasanstypewriter-bold-24";
